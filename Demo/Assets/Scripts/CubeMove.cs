e04484d15baa4c71ae37e0e749862dbd38eb754e
zzz <z2211891492@outlook.com>
2021-01-06 14:26:49 +0800

    Navigation
    实现简单的避障功能

---
+++ Demo/Assets/Scripts/CubeMove.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CubeMove : MonoBehaviour {   
+    private NavMeshAgent meshAgent;
+	void Start () {
+        meshAgent = transform.GetComponent<NavMeshAgent>();      
+	}
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 1 << LayerMask.NameToLayer("Floor"))) {
+                    meshAgent.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+}
